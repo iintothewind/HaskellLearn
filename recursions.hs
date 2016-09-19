c075f86986490667b8ff51795f16277fac09d969
ivar <iintothewind@yeah.net>
2016-09-19 22:51:30 +0800

    updated recursions.hs

--- recursions.hs
+++ recursions.hs
@@ -12,3 +12,17 @@ recmx :: (Ord a) => [a] -> a
 recmx [] = error "max of empty list"
 recmx [x] = x
 recmx (x:rs) = max x (recmx rs)
+
+rptn :: Integer -> x -> [x]
+rptn n x
+    | n <= 0 = []
+    | otherwise = x:rptn (n-1) x
+
+tak :: Integral i => i -> [a] -> [a]
+tak n  _ | n <=0        = []
+tak _ []                = []
+tak n (x:rs)            = x:tak (n-1) rs
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:rs) = rev rs ++ [x]  -- ++ is not an efficient way, and its not stack safe either.
